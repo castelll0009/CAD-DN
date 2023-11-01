7aeec27ce24e5f71ecabbec7686e697829cee092
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2023-11-01 02:37:00 -0400

    deploying

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	site: 'https://castelll0009.github.io',
+	base: '/CAD-DN',
 });
