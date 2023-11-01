574b53dd7512b9ac10bb374bdcc163a331d5d3fc
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2023-11-01 02:45:45 -0400

    after to use tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	site: 'https://castelll0009.github.io',
-	base: '/CAD-DN',
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+  site: 'https://castelll0009.github.io',
+  base: '/CAD-DN'
 });
